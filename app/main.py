9ab79f7457badb7a40a012576239e3f86c7ba543
Ajad <chauhanajad95@gmail.com>
2026-03-20 15:06:57 +0530

    Added Docker setup with optimized Dockerfile and .dockerignore

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+import json
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
@@ -8,17 +11,17 @@ app = FastAPI()
 class Task(BaseModel):
     id: int
     title: str
-    completed: bool = False
+    completed: bool = True
 
 
 # ---- In-memory storage ----
 tasks: List[Task] = []
-
+templates = Jinja2Templates(directory="app/templates")
 
 # ---- Root endpoint ----
 @app.get("/")
-def home():
-    return {"message": "Task Manager API Running"}
+def serve_ui(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 # ---- Get all tasks ----
@@ -32,11 +35,19 @@ def get_tasks():
 def create_task(task: Task):
     tasks.append(task)
     return {"message": "Task added", "task": task}
+#-----patch task as completed ----
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: int):
+    for i in range(len(tasks)):
+        if tasks[i].id == task_id:
+            tasks[i].completed = True
+            return {"message": "Task updated"}
+    return {"error": "Task not found"}
 
 
 # ---- Delete task ----
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
+async def delete_task(task_id: int):
     for task in tasks:
         if task.id == task_id:
             tasks.remove(task)
